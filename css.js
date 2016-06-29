2f7e9768e1275fe1de78d4ee9e0783ee5662e0da
Lem-on <petr.smoldas@centrum.cz>
2016-06-29 19:33:03 +0200

    Update and rename README.md to css.js

---
+++ css.js
@@ -0,0 +1,49 @@
+{
+  "room": "Nečum p*čo hledám kočku",
+  "author": "Lemon",
+  "icon": "http://i.imgur.com/92tLgta.png",
+  "css": null,
+  "rules": {
+    "allowAutorespond": false,
+    "allowAutowoot": true,
+    "allowAutojoin": true,
+    "allowAutograb": true,
+    "allowSmartVote": true,
+    "allowEmotes": true,
+    "allowShowingMehs": false,
+    "forceSmartVote": false
+},
+  "ccc": {
+    "admin": null,
+    "ambassador": null,
+    "host": null,
+    "cohost": null,
+    "manager": null,
+    "bouncer": null,
+    "rdj": null,
+    "subscriber": null,
+    "bronzesubscriber": null,
+    "friend": null,
+    "user": null
+  },
+  "images": {
+    "background": "http://i.imgur.com/IaRSkYZ.jpg", 
+    "playback": "http://i.imgur.com/MOoDUY4.png",
+    "admin": "",
+    "ambassador": "",
+    "host": "http://i.imgur.com/BHT6TDc.png",
+    "cohost": "http://i.imgur.com/BHT6TDc.png",
+    "manager": "http://i.imgur.com/JWrvxMk.png",
+    "bouncer": "http://i.imgur.com/dwJNTj7.png",
+    "rdj": "http://i.imgur.com/oxkUmjt.png",
+    "subscriber": null,
+    "bronzesubscriber": "http://i.imgur.com/pnmHvhM.png",
+    "other": null
+  },
+  "emotes": {
+    "tigerthuglife": "http://i.imgur.com/0GihlO6.png",
+    "tigersex": "http://puu.sh/nIMdE/4e1d31c4cc.gif",
+    "tigerlongnight": "http://puu.sh/nIMqG/d083ac68b7.gif",
+    "tigerwater": "http://puu.sh/nIQUd/116612d5af.gif"
+  }
+}
